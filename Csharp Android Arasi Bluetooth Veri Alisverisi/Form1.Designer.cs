dfbb7d110f94c34f0461ff1561443586d25131c1
Mümin Köykıran <webmaster@catlak-dunya.com>
2016-12-08 23:32:18 +0200

    Add files via upload

---
+++ Csharp Android Arasi Bluetooth Veri Alisverisi/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace Csharp_Android_Arasi_Bluetooth_Veri_Alisverisi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnBring = new System.Windows.Forms.Button();
+            this.cmbSerialPort = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbBaudRate = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbStopBits = new System.Windows.Forms.ComboBox();
+            this.cmbParity = new System.Windows.Forms.ComboBox();
+            this.cmbDataBits = new System.Windows.Forms.ComboBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.txtReceived = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSendData = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnBring
+            // 
+            this.btnBring.Location = new System.Drawing.Point(12, 4);
+            this.btnBring.Name = "btnBring";
+            this.btnBring.Size = new System.Drawing.Size(191, 23);
+            this.btnBring.TabIndex = 0;
+            this.btnBring.Text = "Gerekenleri Getir";
+            this.btnBring.UseVisualStyleBackColor = true;
+            this.btnBring.Click += new System.EventHandler(this.btnBringPort_Click);
+            // 
+            // cmbSerialPort
+            // 
+            this.cmbSerialPort.FormattingEnabled = true;
+            this.cmbSerialPort.Location = new System.Drawing.Point(82, 35);
+            this.cmbSerialPort.Name = "cmbSerialPort";
+            this.cmbSerialPort.Size = new System.Drawing.Size(121, 21);
+            this.cmbSerialPort.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Serial Port:";
+            // 
+            // cmbBaudRate
+            // 
+            this.cmbBaudRate.FormattingEnabled = true;
+            this.cmbBaudRate.Location = new System.Drawing.Point(82, 62);
+            this.cmbBaudRate.Name = "cmbBaudRate";
+            this.cmbBaudRate.Size = new System.Drawing.Size(121, 21);
+            this.cmbBaudRate.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Baud Rate:";
+            // 
+            // cmbStopBits
+            // 
+            this.cmbStopBits.FormattingEnabled = true;
+            this.cmbStopBits.Location = new System.Drawing.Point(82, 89);
+            this.cmbStopBits.Name = "cmbStopBits";
+            this.cmbStopBits.Size = new System.Drawing.Size(121, 21);
+            this.cmbStopBits.TabIndex = 5;
+            // 
+            // cmbParity
+            // 
+            this.cmbParity.FormattingEnabled = true;
+            this.cmbParity.Location = new System.Drawing.Point(82, 116);
+            this.cmbParity.Name = "cmbParity";
+            this.cmbParity.Size = new System.Drawing.Size(121, 21);
+            this.cmbParity.TabIndex = 6;
+            // 
+            // cmbDataBits
+            // 
+            this.cmbDataBits.FormattingEnabled = true;
+            this.cmbDataBits.Location = new System.Drawing.Point(82, 143);
+            this.cmbDataBits.Name = "cmbDataBits";
+            this.cmbDataBits.Size = new System.Drawing.Size(121, 21);
+            this.cmbDataBits.TabIndex = 7;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(209, 4);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(350, 23);
+            this.btnConnect.TabIndex = 8;
+            this.btnConnect.Text = "Bağlan";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // txtSend
+            // 
+            this.txtSend.Location = new System.Drawing.Point(209, 35);
+            this.txtSend.Multiline = true;
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(293, 50);
+            this.txtSend.TabIndex = 9;
+            // 
+            // txtReceived
+            // 
+            this.txtReceived.Location = new System.Drawing.Point(209, 91);
+            this.txtReceived.Multiline = true;
+            this.txtReceived.Name = "txtReceived";
+            this.txtReceived.ReadOnly = true;
+            this.txtReceived.Size = new System.Drawing.Size(293, 73);
+            this.txtReceived.TabIndex = 10;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(508, 91);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(51, 73);
+            this.btnClear.TabIndex = 11;
+            this.btnClear.Text = "Temizle";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnSendData
+            // 
+            this.btnSendData.Location = new System.Drawing.Point(508, 35);
+            this.btnSendData.Name = "btnSendData";
+            this.btnSendData.Size = new System.Drawing.Size(51, 50);
+            this.btnSendData.TabIndex = 12;
+            this.btnSendData.Text = "Veri Gönder";
+            this.btnSendData.UseVisualStyleBackColor = true;
+            this.btnSendData.Click += new System.EventHandler(this.btnSendData_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(101, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(360, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Mümin KÖYKIRAN | www.muminkoykiran.com | muminkoykiran@gmail.com";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 92);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Stop Bits:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 119);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Parity:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 146);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Data Bits:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 192);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnSendData);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtReceived);
+            this.Controls.Add(this.txtSend);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.cmbDataBits);
+            this.Controls.Add(this.cmbParity);
+            this.Controls.Add(this.cmbStopBits);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbBaudRate);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbSerialPort);
+            this.Controls.Add(this.btnBring);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "C# Android Arası Bluetooth Veri Alışverişi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnBring;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbSerialPort;
+        private System.Windows.Forms.ComboBox cmbBaudRate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbStopBits;
+        private System.Windows.Forms.ComboBox cmbParity;
+        private System.Windows.Forms.ComboBox cmbDataBits;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.TextBox txtReceived;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSendData;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Csharp Android Arasi Bluetooth Veri Alisverisi/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.IO.Ports;
+using System.Windows.Forms;
+
+namespace Csharp_Android_Arasi_Bluetooth_Veri_Alisverisi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        SerialPort serialport = new SerialPort();
+
+        public delegate void AddDataDelegate(String myString);
+        public AddDataDelegate myDelegate;
+
+        private void btnBringPort_Click(object sender, EventArgs e)
+        {
+            /* Açık Serial Portları Combobox içerisine yerleştiriyoruz 
+            ve varsayılan olarak ilk portnu seçiyoruz */
+            foreach (string port in SerialPort.GetPortNames())
+            {
+                cmbSerialPort.Items.Add(port);
+            }
+            cmbSerialPort.SelectedIndex = 0;
+
+            /* Tüm olabilecek iletişim hızlarını combobox içerisine yerleştiriyoruz 
+            ve uygun olan varsayılan olarak 9600 seçiyoruz */
+            cmbBaudRate.Items.Add("1200");
+            cmbBaudRate.Items.Add("2400");
+            cmbBaudRate.Items.Add("4800");
+            cmbBaudRate.Items.Add("9600");
+            cmbBaudRate.Items.Add("19200");
+            cmbBaudRate.Items.Add("38400");
+            cmbBaudRate.Items.Add("57600");
+            cmbBaudRate.Items.Add("115200");
+            cmbBaudRate.Items.Add("230400");
+            cmbBaudRate.SelectedIndex = 3;
+
+            //Stop bit ve Party değerlerini combobox içerisine alıp varsılan uygun değerleri seçiyoruz
+            cmbStopBits.DataSource = Enum.GetValues(typeof(StopBits));
+            cmbStopBits.SelectedIndex = 0;
+            cmbParity.DataSource = Enum.GetValues(typeof(Parity));
+            cmbStopBits.SelectedIndex = 1;
+
+            //Data Bit değerlerin combobox içerisine ekliyoruz ve varsayılan olarak 8bit seçiyoruz
+            cmbDataBits.Items.Add("4");
+            cmbDataBits.Items.Add("5");
+            cmbDataBits.Items.Add("6");
+            cmbDataBits.Items.Add("7");
+            cmbDataBits.Items.Add("8");
+            cmbDataBits.SelectedIndex = 4;
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (serialport.IsOpen)
+            {
+                serialport.Close();
+                btnConnect.Text = "Bağlan";
+
+                txtSend.Enabled = false;
+                txtReceived.Enabled = false;
+                btnSendData.Enabled = false;
+                btnClear.Enabled = false;
+            }
+            else if(!serialport.IsOpen)
+            {
+                //Gerekli Olan Ayarları Tanımlıyoruz
+                serialport.PortName = cmbSerialPort.SelectedItem.ToString();
+                serialport.BaudRate = Convert.ToInt16(cmbBaudRate.SelectedItem);
+                serialport.StopBits = (StopBits)cmbStopBits.SelectedItem;
+                serialport.Parity = (Parity)cmbParity.SelectedItem;
+                serialport.DataBits = Convert.ToInt16(cmbDataBits.SelectedItem);
+
+                try
+                {
+                    //Serial Port bağlantısını açıyoruz
+                    serialport.Open();
+                    btnConnect.Text = "Bağlantıyı Kes";
+                    myDelegate = new AddDataDelegate(AddDataMethod);
+                    serialport.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+
+                    txtSend.Enabled = true;
+                    txtReceived.Enabled = true;
+                    btnSendData.Enabled = true;
+                    btnClear.Enabled = true;
+                }
+                catch
+                {
+                    MessageBox.Show("Bağlantı Kurulmadı: \n" + serialport.PortName.ToString() + " " + serialport.BaudRate.ToString());
+                    return;
+                };
+            }
+        }
+        public void AddDataMethod(String myString)
+        {
+            txtReceived.AppendText(myString);
+        }
+        private void DataReceivedHandler(object sender,SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            string s = sp.ReadExisting();
+            txtReceived.Invoke(myDelegate, new Object[] { s });
+        }
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtReceived.Text = "";
+        }
+        private void btnSendData_Click(object sender, EventArgs e)
+        {
+            serialport.WriteLine(txtSend.Text);
+            txtSend.Text = "";
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtSend.Enabled = false;
+            txtReceived.Enabled = false;
+            btnSendData.Enabled = false;
+            btnClear.Enabled = false;
+        }
+    }
+}
